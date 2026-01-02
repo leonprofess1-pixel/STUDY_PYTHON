44e59e75ce66f8050fd2e99e0e63faaf92fb31c5
LEON <leonprofess1@gmail.com>
2026-01-03 08:55:47 +0900

    u

--- 2급 Python_기본/연습.py
+++ 2급 Python_기본/연습.py
@@ -1,12 +1,4 @@
-
-arr = [4,2,1,3,9,5,6]
-
-for i in range(7):
-    a=i
-    for j in range(i,7):
-        if arr[j]<arr[a]:
-                a=j     
-    arr[i], arr[a] = arr[a], arr[i]
-
-print(arr)
-
+for r in range(1,4):
+    for c in range(1,4):
+        print(f"r: {r}, c: {c}")
+        
