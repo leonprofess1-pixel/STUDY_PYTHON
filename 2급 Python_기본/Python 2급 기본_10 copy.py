d96cdf3f9c21aaf4b551112ca9c998b18d827430
LEON <leonprofess1@gmail.com>
2026-01-29 23:42:24 +0900

    s

--- 2급 Python_기본/Python 2급 기본_10 copy.py
+++ 2급 Python_기본/Python 2급 기본_10 copy.py
@@ -1,39 +1,27 @@
 #다음과 같이 import를 사용할 수 있습니다.
 #import math
 
-def solution(arr, N, J):
-    now = 0
-
-    while J > 0:
-        eat = arr.pop(0)
-        J -= eat
-        arr.append(eat)
-        now += 1
-
-        if now > N:
-            now = 1
-
-    return now
+def solution(arr, N):
+    #여기에 코드를 작성해주세요.
+    answer = 0
+    return answer
 
 # 아래는 테스트케이스 출력을 해보기 위한 코드입니다. 아래에는 잘못된 부분이 없으니 위의 코드만 수정하세요.
-arr1 = [7, 5, 2, 9, 3, 4]
-N1 = 6
-J1 = 80
-ret1 = solution(arr1, N1, J1)
+arr1 = [[0, 25, 7, 56, 12], [4, 0, 31, 88, 45], [62, 19, 0, 71, 23], [95, 36, 51, 0, 44], [16, 41, 66, 73, 0]]
+N1 = 5
+ret1 = solution(arr1, N1)
 # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
 print("solution 함수의 반환 값은", ret1, "입니다.")
 
-arr2 = [4, 11, 6, 5, 7, 1, 2, 9, 15]
-N2 = 9
-J2 = 444
-ret2 = solution(arr2, N2, J2)
+arr2 = [[0, 11, 24, 98, 46], [53, 0, 29, 62, 75], [82, 36, 0, 5, 18], [42, 54, 33, 0, 21], [9, 54, 92, 86, 0]]
+N2 = 5
+ret2 = solution(arr2, N2)
 # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
 print("solution 함수의 반환 값은", ret2, "입니다.")
 
-arr3 = [1, 3, 1, 3, 1]
-N3 = 5
-J3 = 131
-ret3 = solution(arr3, N3, J3)
+arr3 = [[0, 18, 34, 22], [48, 0, 55, 61], [18, 7, 0, 15], [71, 86, 94, 0]]
+N3 = 4
+ret3 = solution(arr3, N3)
 # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
 print("solution 함수의 반환 값은", ret3, "입니다.")
 
